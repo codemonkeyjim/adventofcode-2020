a8d10a681318c4cee1ec1b8288ca884bfaba68ca
Jim Pfleger <jpfleger@acm.org>
2020-12-02 10:38:18 -0700

    ✨ Day 2 complete

--- days/day2.py
+++ days/day2.py
@@ -1,31 +1,46 @@
 #!/usr/bin/env python
 
+from collections import Counter
 import pathlib
 import re
-from re import match
-from typing import Dict, List, Optional, TypedDict
+from typing import List, TypedDict
 
 DATA_FILE = 'data/day2.txt'
 
 
 class PasswordPolicy(TypedDict):
-    pattern: re.Pattern
+    lower: int
+    upper: int
+    letter: str
     password: str
 
 
 def parse_line(line: str) -> PasswordPolicy:
     pattern = re.compile(
-        r'(?P<low>\d+)-(?P<high>\d+) (?P<letter>[a-z]): (?P<password>[a-z]+)')
+        r'(?P<lower>\d+)-(?P<upper>\d+) (?P<letter>[a-z]): (?P<password>[a-z]+)')
     matches = pattern.match(line)
     policy: PasswordPolicy = {
-        "pattern": re.compile("{letter}{{{low},{high}}}".format(**matches.groupdict())),
+        "lower": int(matches.group("lower")),
+        "upper": int(matches.group("upper")),
+        "letter": matches.group("letter"),
         "password": matches.group("password")
     }
     return policy
 
 
-def is_password_compliant(policy: PasswordPolicy) -> bool:
-    return bool(policy["pattern"].search(policy["password"]))
+def is_password_count_compliant(policy: PasswordPolicy) -> bool:
+    counts = Counter(policy["password"])
+    return policy["lower"] <= counts.get(policy["letter"], 0) <= policy["upper"]
+
+
+def is_letter_at_position(letter: str, position: int, target: str) -> bool:
+    return position < len(target) and target[position] == letter
+
+
+def is_password_position_compliant(policy: PasswordPolicy) -> bool:
+    matches = [policy[position] for position in ("lower", "upper") if is_letter_at_position(
+        policy["letter"], policy[position] - 1, policy["password"])]
+    return len(matches) == 1
 
 
 def load_file(fn: str = DATA_FILE) -> List[PasswordPolicy]:
@@ -34,5 +49,8 @@ def load_file(fn: str = DATA_FILE) -> List[PasswordPolicy]:
 
 
 if __name__ == "__main__":
-    good_passwords = filter(is_password_compliant, load_file())
-    print(len(list(good_passwords)))
+    password_list = load_file()
+    good_count_passwords = filter(is_password_count_compliant, password_list)
+    print(len(list(good_count_passwords)))
+    good_pos_passwords = filter(is_password_position_compliant, password_list)
+    print(len(list(good_pos_passwords)))
--- tests/test_day2.py
+++ tests/test_day2.py
@@ -5,28 +5,59 @@ from days import day2
 
 
 @pytest.mark.parametrize(
-    "line, pattern, password",
+    "line, lower, upper, letter, password",
     [
-        ["1-4 m: mrfmmbjxr", "m{1,4}", "mrfmmbjxr"],
-        ["5-16 b: bbbbhbbbbpbxbbbcb", "b{5,16}", "bbbbhbbbbpbxbbbcb"],
-        ["13-16 q: qqqqqvzwqqqqqqqq", "q{13,16}", "qqqqqvzwqqqqqqqq"]
+        ["1-4 m: mrfmmbjxr", 1, 4, "m", "mrfmmbjxr"],
+        ["5-16 b: bbbbhbbbbpbxbbbcb", 5, 16, "b", "bbbbhbbbbpbxbbbcb"],
+        ["13-16 q: qqqqqvzwqqqqqqqq", 13, 16, "q", "qqqqqvzwqqqqqqqq"]
     ]
 )
-def test_parse_line(line, pattern, password):
+def test_parse_line(line, lower, upper, letter, password):
     result = day2.parse_line(line)
-    assert result == {"pattern": re.compile(pattern), "password": password}
+    assert result == {"lower": lower, "upper": upper,
+                      "letter": letter, "password": password}
 
 
 @pytest.mark.parametrize(
-    "pattern, password, expected",
+    "lower, upper, letter, password, expected",
     [
-        ["a{1,3}", "abcde", True],
-        ["b{1,3}", "cdefg", False],
-        ["c{2,9}", "ccccccccc", True]
+        [1, 3, "a", "abcde", True],
+        [1, 3, "b", "cdefg", False],
+        [2, 9, "c", "ccccccccc", True],
+        [1, 5, "p", "pppppp", False]
     ]
 )
-def test_is_password_compliant(pattern: str, password: str, expected: bool):
+def test_is_password_count_compliant(lower: int, upper: int, letter: str, password: str, expected: bool):
     policy: day2.PasswordPolicy = {
-        "pattern": re.compile(pattern), "password": password}
-    result = day2.is_password_compliant(policy)
+        "lower": lower, "upper": upper, "letter": letter, "password": password}
+    result = day2.is_password_count_compliant(policy)
+    assert result == expected
+
+
+@pytest.mark.parametrize(
+    "position, letter, target, expected",
+    [
+        [0, "a", "abcde", True],
+        [0, "b", "cdefg", False],
+        [2, "c", "ccccccccc", True],
+        [3, "p", "pp", False]
+    ]
+)
+def test_is_letter_at_position(position: int, letter: str, target: str, expected: bool):
+    assert day2.is_letter_at_position(letter, position, target) == expected
+
+
+@pytest.mark.parametrize(
+    "lower, upper, letter, password, expected",
+    [
+        [1, 3, "a", "abcde", True],
+        [1, 3, "b", "cdefg", False],
+        [2, 9, "c", "ccccccccc", False],
+        [1, 3, "a", "bb", False]
+    ]
+)
+def test_is_password_position_compliant(lower: int, upper: int, letter: str, password: str, expected: bool):
+    policy: day2.PasswordPolicy = {
+        "lower": lower, "upper": upper, "letter": letter, "password": password}
+    result = day2.is_password_position_compliant(policy)
     assert result == expected
