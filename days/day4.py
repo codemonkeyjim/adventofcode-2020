df38ad4a9a2246388a1a381b92a15da1796c3e55
Jim Pfleger <jpfleger@acm.org>
2020-12-04 08:47:43 -0700

    Passport field validators

--- days/day4.py
+++ days/day4.py
@@ -2,6 +2,7 @@
 
 import pathlib
 import re
+from typing import Any, Callable
 
 DATA_FILE = 'data/day4.txt'
 
@@ -9,6 +10,15 @@ DATA_FILE = 'data/day4.txt'
 class Passport:
     REQUIRED_FIELDS = {'byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid'}
     VALID_FIELDS = {'cid'}.union(REQUIRED_FIELDS)
+    VALIDATORS: dict[str, Callable[[Any], bool]] = {
+        'byr': lambda val: 1920 <= int(val) <= 2002,
+        'iyr': lambda val: 2010 <= int(val) <= 2020,
+        'eyr': lambda val: 2020 <= int(val) <= 2030,
+        'hgt': lambda val: (val.endswith("cm") and (150 <= int(val[:-2]) <= 193)) or (val.endswith("in") and (59 <= int(val[:-2]) <= 76)),
+        'hcl': lambda val: re.match(r'#[0-9a-f]{6}', val) is not None,
+        'ecl': lambda val: val in {'amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'},
+        'pid': lambda val: re.match(r'\d{9}', val) is not None
+    }
 
     def __init__(self, text: str) -> None:
         kv_pairs = re.split(r'\s+', text)
@@ -18,8 +28,12 @@ class Passport:
             if k in self.VALID_FIELDS:
                 self.entries[k] = v
 
-    def is_valid(self) -> bool:
+    def has_required_fields(self) -> bool:
         return set(self.entries.keys()).issuperset(self.REQUIRED_FIELDS)
+
+    def is_field_valid(self, field) -> bool:
+        validator = self.VALIDATORS.get(field, lambda _: True)
+        return validator(self.entries[field])
 
 
 def load_file(fn: str = DATA_FILE) -> list[str]:
@@ -30,4 +44,4 @@ def load_file(fn: str = DATA_FILE) -> list[str]:
 
 if __name__ == "__main__":
     passports = [Passport(record) for record in load_file()]
-    print(len([passport for passport in passports if passport.is_valid()]))
+    print(len([passport for passport in passports if passport.has_required_fields()]))
--- tests/test_day4.py
+++ tests/test_day4.py
@@ -27,6 +27,43 @@ def test_passport_init(line, fields):
         ["hcl:#ae17e1 iyr:2013 eyr:2024 ecl:brn pid:760753108 byr:1931 hgt:179cm", True]
     ]
 )
-def test_passport_is_valid(line, valid):
+def test_passport_has_required_fields(line, valid):
     passport = day4.Passport(line)
-    assert passport.is_valid() == valid
+    assert passport.has_required_fields() == valid
+
+
+@pytest.mark.parametrize(
+    "field,value,valid",
+    [
+        ["byr", "1920", True],
+        ["byr", "1919", False],
+        ["byr", "2002", True],
+        ["byr", "2003", False],
+        ["iyr", "2010", True],
+        ["iyr", "2009", False],
+        ["iyr", "2020", True],
+        ["iyr", "2021", False],
+        ["eyr", "2020", True],
+        ["eyr", "2019", False],
+        ["eyr", "2030", True],
+        ["eyr", "2031", False],
+        ["hgt", "150cm", True],
+        ["hgt", "149cm", False],
+        ["hgt", "193cm", True],
+        ["hgt", "194cm", False],
+        ["hgt", "59in", True],
+        ["hgt", "58in", False],
+        ["hgt", "76in", True],
+        ["hgt", "77in", False],
+        ["hcl", "#123abc", True],
+        ["hcl", "#123abz", False],
+        ["hcl", "123abc", False],
+        ["ecl", "brn", True],
+        ["ecl", "wat", False],
+        ["pid", "000000001", True],
+        ["pid", "0123456789", False],
+    ]
+)
+def test_passsport_field_validators(field, value, valid):
+    passport = day4.Passport(f"{field}:{value}")
+    assert passport.is_field_valid(field) == valid
