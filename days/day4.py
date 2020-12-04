5579021735cfeec20f2ac4a3555d816a737de32f
Jim Pfleger <jpfleger@acm.org>
2020-12-04 07:53:53 -0700

    ✨ Day 4 part 1 done

--- days/day4.py
+++ days/day4.py
@@ -7,8 +7,8 @@ DATA_FILE = 'data/day4.txt'
 
 
 class Passport:
-    REQUIRED_FIELDS = ('byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid')
-    VALID_FIELDS = ('cid',) + REQUIRED_FIELDS
+    REQUIRED_FIELDS = {'byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid'}
+    VALID_FIELDS = {'cid'}.union(REQUIRED_FIELDS)
 
     def __init__(self, text: str) -> None:
         kv_pairs = re.split(r'\s+', text)
@@ -17,3 +17,17 @@ class Passport:
             k, v = pair.split(':', maxsplit=1)
             if k in self.VALID_FIELDS:
                 self.entries[k] = v
+
+    def is_valid(self) -> bool:
+        return set(self.entries.keys()).issuperset(self.REQUIRED_FIELDS)
+
+
+def load_file(fn: str = DATA_FILE) -> list[str]:
+    with pathlib.Path(fn).open() as in_file:
+        whole_file = in_file.read()
+    return re.split(r'\n\n', whole_file)
+
+
+if __name__ == "__main__":
+    passports = [Passport(record) for record in load_file()]
+    print(len([passport for passport in passports if passport.is_valid()]))
--- tests/test_day4.py
+++ tests/test_day4.py
@@ -17,3 +17,16 @@ hcl:#cfa07d byr:1929""", ['iyr', 'ecl', 'cid', 'eyr', 'pid', 'hcl', 'byr']]
 def test_passport_init(line, fields):
     passport = day4.Passport(line)
     assert set(passport.entries.keys()) == set(fields)
+
+
+@pytest.mark.parametrize(
+    "line,valid",
+    [
+        ["ecl:gry pid:860033327 eyr:2020 hcl:#fffffd byr:1937 iyr:2017 cid:147 hgt:183cm", True],
+        ["iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884 hcl:#cfa07d byr:1929",  False],
+        ["hcl:#ae17e1 iyr:2013 eyr:2024 ecl:brn pid:760753108 byr:1931 hgt:179cm", True]
+    ]
+)
+def test_passport_is_valid(line, valid):
+    passport = day4.Passport(line)
+    assert passport.is_valid() == valid
